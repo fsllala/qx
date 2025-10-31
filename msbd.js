dc2299a120204ddfe5445f88252d11c2ee8a59d7
fsllala <44030722+fsllala@users.noreply.github.com>
2025-11-01 00:57:57 +0800

    Modify VIP status and expiration in response

--- msbd.js
+++ msbd.js
@@ -1,9 +1,10 @@
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
-const vip = '/account/me';
-if (url.indexOf(vip) != -1) {
-    obj.data.vip = true;
-    body = JSON.stringify(obj);
-}
+
+obj.data.$isVIP = true;
+obj.data.$isValidVip=true;
+obj.data.purchaseLength="lifelong";
+obj.data.vipExpiration="2029-12-31 23:59:59";
+    body = JSON.stringify(obj)
 $done({body});
