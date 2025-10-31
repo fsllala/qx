4f56b3472758005caf7cc77d15423180979fc559
fsllala <44030722+fsllala@users.noreply.github.com>
2025-11-01 07:01:39 +0800

    and key

--- msbd.js
+++ msbd.js
@@ -5,6 +5,7 @@ var obj = JSON.parse(body);
 obj.data.$isVIP = true;
 obj.data.$isValidVip=true;
 obj.data.purchaseLength="lifelong";
+obj.data.customerKey="cu_1f233dda-fff6-4d79-af3f-c1afc1f01fd2";
 obj.data.vipExpiration="2029-12-31 23:59:59";
     body = JSON.stringify(obj)
 $done({body});
